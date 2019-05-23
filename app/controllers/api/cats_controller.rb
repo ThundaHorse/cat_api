51a14811d338cfb894013f0e9592dbd05f70aa2a
ThundaHorse <abeeeeekim@gmail.com>
2019-05-22 22:17:57 -0500

    added image tag to show image from url via html.erb

--- app/controllers/api/cats_controller.rb
+++ app/controllers/api/cats_controller.rb
@@ -1,7 +1,8 @@
 class Api::CatsController < ApplicationController
   def index 
-    response = HTTP.get("https://api.thecatapi.com/v1/images/search?breed_ids=bengal&api_key=#{ENV['API_KEY']}")
-      
-    render json: response.parse 
+    @response = HTTP.get("https://api.thecatapi.com/v1/images/search?&api_key=#{ENV['API_KEY']}")
+    
+    render 'index.html'
   end 
 end
+
